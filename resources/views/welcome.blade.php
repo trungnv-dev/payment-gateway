022f9b842c2490fd25f3eb24c01853a5f9efdab6
trungnv <nguyenvantrung31121998@gmail.com>
2022-07-28 04:32:55 +0000

    remove class hidden

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <a href="{{ route('payment.gmo.index') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Gmo Payment</a>
                         <a href="{{ url('/export-user') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Export User</a>
